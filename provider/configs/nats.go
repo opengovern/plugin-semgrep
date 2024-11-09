68e71d763b5dc721d431c74a48c822e7e25c8db0
artaasadi <arta-c@kaytu.io>
2024-11-09 01:08:19 +0100

    fix: fix discriber

--- provider/configs/nats.go
+++ provider/configs/nats.go
@@ -1,9 +1,9 @@
 package configs
 
 const (
-	StreamName           = "og_azure_describer"
-	JobQueueTopic        = "og_azure_describer_job_queue"
-	ConsumerGroup        = "azure-describer"
-	JobQueueTopicManuals = "og_azure_describer_manuals_job_queue"
-	ConsumerGroupManuals = "azure-describer-manuals"
+	StreamName           = "stream_name"
+	JobQueueTopic        = "job_queue_topic"
+	ConsumerGroup        = "consumer-group"
+	JobQueueTopicManuals = "job_queue_topic_manuals"
+	ConsumerGroupManuals = "consumer-group-manuals"
 )
