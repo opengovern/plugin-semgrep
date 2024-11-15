5ae32b59fe71a851cc94c7d723061db337d78f0a
Mahan Zendedel DH <mahanmmi@gmail.com>
2024-11-15 15:20:27 +0400

    fix: model autogen script

--- provider/model/model.go
+++ provider/model/model.go
@@ -1,4 +1,4 @@
-//go:generate go run ../../SDK/runnable/models/main.go --file $GOFILE --output ../../SDK/generated/resources_clients.go --type $PROVIDER
+//go:generate go run ../../pkg/sdk/runable/steampipe_es_client_generator/main.go -pluginPath ../../steampipe-plugin-REPLACEME/REPLACEME -file $GOFILE -output ../../pkg/sdk/es/resources_clients.go -resourceTypesFile ../resource_types/resource-types.json
 
 // Implement types for each resource
 
