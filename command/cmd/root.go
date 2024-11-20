91e99f95e12be1bf87b6a71a752e5316c600ad36
artaasadi <arta-c@kaytu.io>
2024-11-20 10:37:04 +0100

    fix: add command for testing

---
+++ command/cmd/root.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "run",
+	Short: "OpenGovernance aws describer manual",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var items []string
+		items = append(items, "describer")
+
+		return describerCmd.Help()
+
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(describerCmd)
+}
