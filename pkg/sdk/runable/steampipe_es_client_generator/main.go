1b19208166ce2c7a49c3cc9dabfc4ac8ba7ceaa1
artaasadi <arta-c@kaytu.io>
2024-11-29 11:44:26 +0100

    fix: fix autogenerate code

--- pkg/sdk/runable/steampipe_es_client_generator/main.go
+++ pkg/sdk/runable/steampipe_es_client_generator/main.go
@@ -399,12 +399,9 @@ func Get{{ .Name }}(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateD
 		fmt.Fprintln(&buf, `
 		import (
 			"context"
-			"encoding/json"
-			"fmt"
 			essdk "github.com/opengovern/og-util/pkg/opengovernance-es-sdk"
 			steampipesdk "github.com/opengovern/og-util/pkg/steampipe"
 			"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-			`+configs.IntegrationTypeLower+`Describer "`+configs.OGPluginRepoURL+`/provider/describer"
 			`+configs.IntegrationTypeLower+` "`+configs.OGPluginRepoURL+`/provider/model"
             "runtime"
 		)
