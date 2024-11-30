2c7b98db5e13dfa184585822cce2a8c71f11111d
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-11-30 22:39:37 +0330

    feat: change kaytu name

--- pkg/describer/handler.go
+++ pkg/describer/handler.go
@@ -44,7 +44,7 @@ func getJWTAuthToken() (string, error) {
 	}
 
 	token, err := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
-		"email": "describe-worker@kaytu.io",
+		"email": "describe-worker@opencomply.io",
 	}).SignedString(pk)
 	if err != nil {
 		return "", fmt.Errorf("JWT token generation failed %v", err)
