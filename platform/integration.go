4faa25d5a9fd1dc62cb04244aa374c03be9415df
Mahan Zendedel DH <mahanmmi@gmail.com>
2025-01-17 20:00:14 +0400

    feat: add plugin ping

--- platform/integration.go
+++ platform/integration.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/opengovern/og-describer-template/platform/constants"
 	"github.com/opengovern/og-describer-template/global"
 	"github.com/opengovern/og-describer-template/global/maps"
+	"github.com/opengovern/og-describer-template/platform/constants"
 	"github.com/opengovern/og-util/pkg/integration"
 	"github.com/opengovern/og-util/pkg/integration/interfaces"
 )
 
 type Integration struct{}
 
-func (i *Integration) GetConfiguration() interfaces.IntegrationConfiguration {
+func (i *Integration) GetConfiguration() (interfaces.IntegrationConfiguration, error) {
 	return interfaces.IntegrationConfiguration{
 		NatsScheduledJobsTopic:   global.JobQueueTopic,
 		NatsManualJobsTopic:      global.JobQueueTopicManuals,
@@ -27,7 +27,7 @@ func (i *Integration) GetConfiguration() interfaces.IntegrationConfiguration {
 
 		DescriberDeploymentName: constants.DescriberDeploymentName,
 		DescriberRunCommand:     constants.DescriberRunCommand,
-	}
+	}, nil
 }
 
 func (i *Integration) HealthCheck(jsonData []byte, providerId string, labels map[string]string, annotations map[string]string) (bool, error) {
@@ -37,9 +37,8 @@ func (i *Integration) HealthCheck(jsonData []byte, providerId string, labels map
 		return false, err
 	}
 	// TODO add credentials
-	isHealthy, err := IntegrationHealthcheck(Config{
-	})
-	
+	isHealthy, err := IntegrationHealthcheck(Config{})
+
 	return isHealthy, err
 }
 
@@ -51,9 +50,8 @@ func (i *Integration) DiscoverIntegrations(jsonData []byte) ([]integration.Integ
 	}
 	var integrations []integration.Integration
 	// TODO
-	_, err = IntegrationDiscovery(Config{
-	})
-	
+	_, err = IntegrationDiscovery(Config{})
+
 	return integrations, nil
 }
 
@@ -69,19 +67,19 @@ func (i *Integration) GetResourceTypesByLabels(labels map[string]string) (map[st
 	return resourceTypesMap, nil
 }
 
-func (i *Integration) GetResourceTypeFromTableName(tableName string) string {
+func (i *Integration) GetResourceTypeFromTableName(tableName string) (string, error) {
 	if v, ok := maps.TablesToResourceTypes[tableName]; ok {
-		return v
+		return v, nil
 	}
 
-	return ""
+	return "", nil
 }
 
-func (i *Integration) GetIntegrationType() integration.Type {
-	return constants.IntegrationName
+func (i *Integration) GetIntegrationType() (integration.Type, error) {
+	return constants.IntegrationName, nil
 }
 
-func (i *Integration) ListAllTables() map[string][]interfaces.CloudQLColumn {
+func (i *Integration) ListAllTables() (map[string][]interfaces.CloudQLColumn, error) {
 	plugin := global.Plugin()
 	tables := make(map[string][]interfaces.CloudQLColumn)
 	for tableKey, table := range plugin.TableMap {
@@ -92,5 +90,9 @@ func (i *Integration) ListAllTables() map[string][]interfaces.CloudQLColumn {
 		tables[tableKey] = columns
 	}
 
-	return tables
+	return tables, nil
+}
+
+func (i *Integration) Ping() error {
+	return nil
 }
