d813cae584d62f1be959bc8122461f0277a00145
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2025-01-21 16:40:05 +0330

    feat: update manifest file

--- platform/constants/update-manifest.sh
+++ platform/constants/update-manifest.sh
@@ -4,7 +4,10 @@ file_path="manifest.yaml"
 
 # Append multiple lines to the file
 cat <<EOF >> "$file_path"
+
 DescriberTag: local-$TAG
+UpdateDate: $(date +%Y-%m-%d)
+
 EOF
 
 echo "Data has been appended to $file_path"
